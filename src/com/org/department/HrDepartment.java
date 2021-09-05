0b196cfaa97357c43330ad4c170b42e1351d03f9
Bikrammam <89743935+Bikrammam@users.noreply.github.com>
2021-09-05 12:55:25 -0400

    Add files via upload

---
+++ src/com/org/department/HrDepartment.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.org.department;
+
+/**
+ *
+ */
+public class HrDepartment extends SuperDepartment {
+
+	public String departmentName() {
+		return "Hr Department";
+	}
+
+	public String getTodaysWork() {
+		return "Fill today's worksheet and mark your attaendance";
+	}
+
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+
+	public String doActivity() {
+		return "team Lunch";
+	}
+
+}
